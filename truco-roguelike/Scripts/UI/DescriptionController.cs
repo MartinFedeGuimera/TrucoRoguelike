3360dd234521859522a1c6d8636c4d86e6de2ca5
Martin Guimera <martinfedeguimera2009@gmail.com>
2026-05-11 21:41:59 -0300

    fix: Descriptions addaptables
    Now the size of the Panel in the Description scene adapts to the size of the Description text. Also, now the Description moves to be visible when a part of it its outside the screen.

--- truco-roguelike/Scripts/UI/DescriptionController.cs
+++ truco-roguelike/Scripts/UI/DescriptionController.cs
@@ -4,6 +4,7 @@ public partial class DescriptionController : Control
 {
     private Label title;
     private Label description;
+    private MarginContainer margin;
 
     private bool isVisible = false;
 
@@ -11,31 +12,43 @@ public partial class DescriptionController : Control
 
     public void SetUp()
     {
+        SetAnchorsPreset(LayoutPreset.TopLeft);
+
         title = GetNode<Label>("MarginContainer/VBoxContainer/Title");
         description = GetNode<Label>("MarginContainer/VBoxContainer/Description");
+        margin = GetNode<MarginContainer>("MarginContainer");
 
         description.AutowrapMode = TextServer.AutowrapMode.Word;
-
         description.CustomMinimumSize = new Vector2(maxDescriptionWidth, 0);
-
-        description.SizeFlagsHorizontal = SizeFlags.ShrinkBegin;
-        description.SizeFlagsVertical = SizeFlags.ShrinkBegin;
     }
 
-    public void ChangeData(string titleText, string descriptionText)
+    public async void ChangeData(string titleText, string descriptionText)
     {
         title.Text = titleText;
         description.Text = descriptionText;
+
+        await ToSignal(GetTree(), SceneTree.SignalName.ProcessFrame);
+
+        ResetSize();
     }
 
     public override void _Process(double delta)
     {
-        if(isVisible)
-        {
-            Vector2 mousePos = GetViewport().GetMousePosition();
+        if (!isVisible)
+            return;
 
-            GlobalPosition = mousePos + new Vector2(16, 16);
-        }
+        Vector2 mousePos = GetViewport().GetMousePosition();
+        Vector2 viewportSize = GetViewportRect().Size;
+
+        Vector2 pos = mousePos + new Vector2(16, 16);
+
+        if (pos.X + margin.Size.X > viewportSize.X)
+            pos.X = mousePos.X - margin.Size.X - 16;
+
+        if (pos.Y + margin.Size.Y > viewportSize.Y)
+            pos.Y = mousePos.Y - margin.Size.Y - 16;
+
+        GlobalPosition = pos;
     }
 
     public void OnShow()
